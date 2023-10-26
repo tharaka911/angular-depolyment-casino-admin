1e2de50a0c45033c1ac0fc92029f30195f24c1fa
tharaka911 <tharakahitibandara@gmail.com>
2023-10-26 10:53:52 +0530

    ok

---
+++ fresh-start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#go to the project directory
+cd /var/www/html/casino-new/casino-admin
+
+#remove the servercode changes 
+git restore .
+
+# Pull the latest changes from the git repository
+git pull
+
+# Stop and delete the PM2 process
+pm2 delete casino-admin
+
+# Navigate to the .angular directory
+cd .angular/
+
+# Remove the cache directory and its contents
+rm -r cache/
+
+# Return to the parent directory
+cd ..
+
+# Remove the dist directory and its contents
+rm -r dist/
+
+# Run the NPM build script
+npm run build
+
+# Start a new PM2 process with the name "casino-admin"
+pm2 start "npm run start" --name casino-admin
+
+# Optionally, display a message when the script is done
+echo "Deployment script completed."
---
+++ readme.md
@@ -0,0 +1,16 @@
+## Steps for angular fresh start
+
+### clone me
+```sh
+git clone 
+```
+
+### give me access to execute
+```sh
+chmod +x deploy.sh
+```
+
+### run me
+```sh
+bash -x ./deploy.sh
+```
